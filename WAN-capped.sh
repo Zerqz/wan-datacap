33eaf8cee5408ac91965a289d8f6fa794d63d3e2
Zerqz <Zerqz@users.noreply.github.com>
2017-10-06 12:21:46 -0600

    Update and rename WANdown-Capped.sh to WAN-capped.sh

--- WAN-capped.sh
+++ WAN-capped.sh
@@ -1,6 +1,8 @@
-#!/bin/bash  
+#!/bin/bash
+
 #Running on a pfSense box with WAN as 4G, and opt1 as DSL fail-over after cap has been reached.
 #Required pfSense packages: "sudo","bandwidthd","cron'
+
 
 #Run as cronjob on the 1st of each month, or when cap has been reached (running every 5 minutes to check if close to cap)
 
